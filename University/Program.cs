6813728ef4a7c30b42d988f7ff7e927caab22f47
clackgot <clackgot@gmail.com>
2021-04-28 17:48:12 +0300

    Добавлены комментарии

--- University/Program.cs
+++ University/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace University
 {
-
+    /// <summary>
+    /// Пол
+    /// </summary>
     public enum Gender
     {
         Male,
@@ -14,22 +16,19 @@ namespace University
     }
     class Person
     {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// Пол
+        /// </summary>
+        public Gender Sex { get; set; }
 
-        private string name;
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        private int age;
-        public int Age
-        {
-            get { return age; }
-            set { age = value; }
-        }
-
-        private Gender sex;
 
         public Person(string name, int age, Gender sex)
         {
@@ -38,18 +37,16 @@ namespace University
             Sex = sex;
         }
 
-        public Gender Sex
-        {
-            get { return sex; }
-            set { sex = value; }
-        }
 
-
+        /// <summary>
+        /// Лет до пенсии
+        /// </summary>
+        /// <returns></returns>
         public int GetYearsBeforeRetirementAge()
         {
             if(Sex == Gender.Female)
             {
-                int ageDifference = 57 - age;
+                int ageDifference = 57 - Age;
                 if (ageDifference > 0)
                 {
                     return ageDifference;
@@ -61,7 +58,7 @@ namespace University
             }
             else if(Sex == Gender.Male)
             {
-                int ageDifference = 62 - age;
+                int ageDifference = 62 - Age;
                 if (ageDifference > 0)
                 {
                     return ageDifference;
@@ -73,7 +70,7 @@ namespace University
             }
             else
             {
-                int ageDifference = 60 - age;
+                int ageDifference = 60 - Age;
                 if (ageDifference > 0)
                 {
                     return ageDifference;
@@ -84,119 +81,141 @@ namespace University
                 }
             }
         }
+        /// <summary>
+        /// Период возраста
+        /// </summary>
+        /// <returns></returns>
         public string GetAgePeriodization()
         {
-            if (age == 0) return "Грудной ребёнок";
-            if (age == 1 || age == 2) return "Раннее детство";
-            if (age >= 3 && age <= 7) return "Первый период детства";
+            if (Age == 0) return "Грудной ребёнок";
+            if (Age == 1 || Age == 2) return "Раннее детство";
+            if (Age >= 3 && Age <= 7) return "Первый период детства";
 
             if(Sex == Gender.Male)
             {
-                if (age >= 8 && age <= 12) return "Второй период детства";
-                if (age >= 13 && age <= 16) return "Подростковый возраст";
-                if (age >= 17 && age <= 21) return "Юношеский возраст";
-                if (age >= 22 && age <= 60) return "Средний возраст";
-                if (age >= 61 && age <= 75) return "Пожилой человек";
+                if (Age >= 8 && Age <= 12) return "Второй период детства";
+                if (Age >= 13 && Age <= 16) return "Подростковый возраст";
+                if (Age >= 17 && Age <= 21) return "Юношеский возраст";
+                if (Age >= 22 && Age <= 60) return "Средний возраст";
+                if (Age >= 61 && Age <= 75) return "Пожилой человек";
             }
 
             if (Sex == Gender.Female)
             {
-                if (age >= 8 && age <= 11) return "Второй период детства";
-                if (age >= 12 && age <= 15) return "Подростковый возраст";
-                if (age >= 16 && age <= 20) return "Юношеский возраст";
-                if (age >= 21 && age <= 55) return "Средний возраст";
-                if (age >= 56 && age <= 75) return "Пожилой человек";
+                if (Age >= 8 && Age <= 11) return "Второй период детства";
+                if (Age >= 12 && Age <= 15) return "Подростковый возраст";
+                if (Age >= 16 && Age <= 20) return "Юношеский возраст";
+                if (Age >= 21 && Age <= 55) return "Средний возраст";
+                if (Age >= 56 && Age <= 75) return "Пожилой человек";
             }
-            if (age >= 76 && age <= 90) return "Старческий возраст";
-            if (age > 90) return "Долгожитель";
+            if (Age >= 76 && Age <= 90) return "Старческий возраст";
+            if (Age > 90) return "Долгожитель";
 
             return "Неверный возраст";
         }
+        /// <summary>
+        /// Год рождения
+        /// </summary>
+        /// <returns></returns>
         public int YearOfBirth()
         {
-            return DateTime.Now.Year - age;
+            return DateTime.Now.Year - Age;
         }
     }
 
-    public enum AcademicPosition
-    {
-        GraduateStudent,
-        ResearchAssociate,
-        Assistant,
-        Teacher,
-        LeadingResearcher,
-        Professor,
-        ChiefResearchOfficer,
-        SeniorLecturer,
-        DoctoralStudent,
-        Trainee,
-        AssociateProfessor,
-        SeniorResearcher,
-        JuniorResearcher
-    }
+
+    /// <summary>
+    /// Преподаватель
+    /// </summary>
     class Teacher : Person
     {
-        private int workExperience;
+        /// <summary>
+        /// Стаж работы
+        /// </summary>
+        public int WorkExperience { get; set; }
 
-        public int WorkExperience
-        {
-            get { return workExperience; }
-            set { workExperience = value; }
-        }
+        /// <summary>
+        /// Научных работ
+        /// </summary>
+        public int ScientificPapersCount { get; set; }
 
-        private int scientificPapersCount;
+        public int EducatesStudents { get; set; }
 
-        public int ScientificPapersCount
-        {
-            get { return scientificPapersCount; }
-            set { scientificPapersCount = value; }
-        }
-
-        private int educatesStudents;
-
-        public int EducatesStudents
-        {
-            get { return educatesStudents; }
-            set { educatesStudents = value; }
-        }
-
-
+        /// <summary>
+        /// Отношение стажа работы к возрасту
+        /// </summary>
+        /// <returns></returns>
         public double GetPercentageOfWorkExperienceByAge()
         {
             return ((double)(WorkExperience) / Age) * 100;
         }
+        /// <summary>
+        /// Научных работ в год за период работы
+        /// </summary>
+        /// <returns></returns>
         public double GetScientificPapersPerWorkYear()
         {
             return (double)ScientificPapersCount / WorkExperience;
         }
+        /// <summary>
+        /// Год начала работы
+        /// </summary>
+        /// <returns></returns>
         public int GetCareerStartYear()
         {
             return DateTime.Now.Year - WorkExperience;
         }
 
-        public Teacher(string name, int age, Gender sex, int workExperience, int scientificPapersCount, AcademicPosition position) : base(name, age, sex)
+        /// <summary>
+        /// Преподаватель
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="sex">Пол</param>
+        /// <param name="workExperience">Стаж работы</param>
+        /// <param name="scientificPapersCount">Кол-во научных работ</param>
+        /// <param name="educatesStudents">Кол-во обучаемых студентов</param>
+        public Teacher(string name, int age, Gender sex, int workExperience, int scientificPapersCount, int educatesStudents) : base(name, age, sex)
         {
             WorkExperience = workExperience;
             ScientificPapersCount = scientificPapersCount;
-            Position = position;
+            EducatesStudents = educatesStudents;
         }
 
 
     }
     class HeadOfDepartment
     {
-        private int subordinateTeachers;
-
-        public int SubordinateTeachers
-        {
-            get { return subordinateTeachers; }
-            set { subordinateTeachers = value; }
-        }
+        /// <summary>
+        /// Преподавателей в подчинении
+        /// </summary>
+        public int SubordinateTeachers { get; set; }
+        /// <summary>
+        /// Издано учебных материалов
+        /// </summary>
+        public int TextbooksPublished { get; set; }
+        /// <summary>
+        /// Сумма полученных грантов
+        /// </summary>
+        public double AmountOfGrantsReceived { get; set; }
 
     }
     class Student
     {
+        /// <summary>
+        /// Средняя оценка
+        /// </summary>
+        public double AverageRating { get; set; }
 
+        /// <summary>
+        /// Год обучения
+        /// </summary>
+        public int CourseNumber { get; set; }
+
+        /// <summary>
+        /// Обучение на коммерческой основе
+        /// </summary>
+        public bool Commerce { get; set; }
     }
 
     class Program
